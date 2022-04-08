a09129629abe47ef2109de864c36dbf4cf9554f8
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2022-04-08 10:19:01 -0500

    Add files via upload

--- Final Project/src/Piece.java
+++ Final Project/src/Piece.java
@@ -1,9 +1,9 @@
-//Rook   0  Move {{0,1}{0-1}{-1,0}{1,0}}  |  Castling
-//Bishop 1  Move {{+-1, +-1}
-//Queen  2  Move (Rook + Pawn)
-//Knight 3
-//Pawn   4
-//King   5
+//Rook   0  Move {{0,1}{0-1}{-1,0}{1,0}}  |  Castling        4
+//Bishop 1  Move {{+-1, +-1}    3
+//Queen  2  Move (Rook + Pawn)    9
+//Knight 3    3
+//Pawn   4  Move | double move | take | Promotion | En passant   1
+//King   5   100000
 
 
 import java.util.ArrayList;
@@ -11,46 +11,86 @@ import java.util.ArrayList;
 public class Piece {
     private int x;
     private int y;
-    private int score;
+    //private int score;
     private int thisPiece;
-    private boolean color;
+    private final boolean isWhite;
+    private Board board;
 
-    public Piece(int p, int x, int y, boolean isWhite){
+    public Piece(int piece, int x, int y, boolean isWhite, Board board){
         this.x = x;
         this.y = y;
-        score = 3;
-        thisPiece = p; // {bishop, rook, queen, king, pawn, knight}
-        color = isWhite;
-    }
-    
-    public int getIndex(){
-        return thisPiece;
-    }
-    public boolean getColor() {
-        return color;
+        thisPiece = piece; // {rook, bishop, queen, knight, pawn, king}
+        this.isWhite = isWhite;
+        this.board = board;
     }
 
-    public ArrayList<ArrayList<Integer>> rookCanMove(){
+    public boolean isWhite(){
+        return isWhite;
+    }
+
+
+    private ArrayList<Integer[]> rookCanMove(){
+        ArrayList<Integer[]> result = new ArrayList<>();
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}}; //List of directions to go in
+        for(int[] offset: directions) {
+            for(int j = 1; j < 8; j ++){
+                Integer[] coords = {x + offset[0], y + offset[1]};
+                if( (coords[0] < 8 && coords[0] > -1) && (coords[1] < 8 && coords[1] > -1)){
+                    Piece p = board.getPiece(coords[0],coords[1]);
+                    if(p == null){
+                        result.add(coords);
+                    } else { //If there is a piece it can't hop over it
+                        j = 8;
+                        if(p.isWhite() != isWhite){
+                            result.add(coords); //It can capture it though
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+    private ArrayList<Integer[]> bishopCanMove(){
+        ArrayList<Integer[]> result = new ArrayList<>();
+        int[][] directions = {{1,1},{-1,-1},{1,-1},{-1,1}}; //List of directions to go in
+        for(int[] offset: directions) {
+            for(int j = 1; j < 8; j ++){
+                Integer[] coords = {x + offset[0], y + offset[1]};
+                if( (coords[0] < 8 && coords[0] > -1) && (coords[1] < 8 && coords[1] > -1)){
+                    Piece p = board.getPiece(coords[0],coords[1]);
+                    if(p == null){
+                        result.add(coords);
+                    } else {
+                        j = 8;
+                        if(p.isWhite() != isWhite){
+                            result.add(coords);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Integer[]> knightCanMove(){
         //TODO Make function
         return null;
     }
 
-    public ArrayList<ArrayList<Integer>> bishopCanMove(){
+    private ArrayList<Integer[]> pawnCanMove(){
         //TODO Make function
         return null;
     }
 
-    public ArrayList<ArrayList<Integer>> knightCanMove(){
+    private ArrayList<Integer[]> kingCanMove(){
         //TODO Make function
         return null;
     }
 
-    public ArrayList<ArrayList<Integer>> pawnCanMove(){
-        //TODO Make function
-        return null;
-    }
-
-    public ArrayList<ArrayList<Integer>> pieceCanMove(){
+    public ArrayList<Integer[]> pieceCanMove(){
         if(thisPiece == 0){
             //Rook
             bishopCanMove();
@@ -58,8 +98,8 @@ public class Piece {
             //Bishop
         } else if(thisPiece == 2){
             //Queen
-            ArrayList<ArrayList<Integer>> result = rookCanMove();
-            ArrayList<ArrayList<Integer>> result2 = bishopCanMove();
+            ArrayList<Integer[]> result = rookCanMove();
+            ArrayList<Integer[]> result2 = bishopCanMove();
             for(int i = 0; i < result2.size(); i ++){
                 result.add(result2.get(i));
             }
@@ -73,7 +113,4 @@ public class Piece {
         }
         return null;
     }
-
-
-
 }
