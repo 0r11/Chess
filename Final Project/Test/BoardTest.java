98596a4a03671e88ca916a056162ebc1c5591d89
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2022-04-20 18:57:50 -0500

    Add files via upload

---
+++ Final Project/Test/BoardTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+    private final Board b = new Board();
+
+    @Test
+    public void initialize(){
+        b.initialize();
+        assertEquals(b.getPiece(0,0).getPiece(), 0);
+    }
+    @Test
+    public void RookCantMove(){
+        b.initialize(); //Can't move at start of game
+        assertEquals(b.getPiece(0,0).pieceCanMove(), new ArrayList<Integer[]>());
+    }
+
+    
+
+
+}
